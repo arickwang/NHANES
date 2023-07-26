09c710d849b8401b490da02565a0b554a5ade63c
Wang <pyq3@cdc.gov>
2023-07-26 13:51:43 -0400

    Added function table_prevlance

--- help_functions.R
+++ help_functions.R
@@ -11,3 +11,22 @@ makePretty <- function(mean,lower,upper,sig = "%.1f") {
   
   return(pretty)
 }
+
+tab_prevs <- function(svyobj, pivot, label = NULL, sig = "%.1f") {
+  pivot = rlang::enquo(pivot)
+  
+  if(is.null(label)) {label = pivot} else {label = rlang::enquo(label)}
+  
+  
+  
+  tmp <- svyobj %>% 
+    group_by(!!pivot) %>%
+    summarise(n = unweighted(n()),
+              mean = survey_mean(vartype = "ci")*100) %>%
+    mutate(group = !!pivot,
+           mean2CI = makePretty(mean, mean_low, mean_upp, sig = sig)) %>%
+    select(group, n, mean2CI) %>%
+    rename(!!label := group)
+  
+  return(tmp)
+}
